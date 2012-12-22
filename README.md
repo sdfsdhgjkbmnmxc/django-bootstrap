7142ad616d9305ef2d3977c59e394b2c702e5c6a
Maxim Oransky <om@ostrovok.ru>
2012-12-22 19:47:17 +0400

    initial

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 django-bootstrap
 ================
+
+Install
+```
+pip install -e git+git://github.com/sdfsdhgjkbmnmxc/django-bootstrap.git#egg=djbootstrap
+```
+
+Setup settings.py
+```python 
+INSTALLED_APPS = (
+    # ....
+    'djbootstrap',
+)
+```
+
---
+++ djbootstrap/templatetags/djbootstrap.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+from django.forms.widgets import CheckboxInput
+from django.template import Library
+from django.utils.safestring import mark_safe
+
+
+register = Library()
+
+
+@register.inclusion_tag('bootstrap/form.html')
+def show_form(form):
+    return _show_form(form)
+
+
+@register.inclusion_tag('bootstrap/wide_form.html')
+def show_wide_form(form):
+    return _show_form(form)
+
+
+def _show_form(form):
+    fields = []
+    for field in form:
+        field.is_checkbox = isinstance(field.field.widget, CheckboxInput)
+        bits = field.label_tag().split('>', 1)
+        field.label_attrs = mark_safe(bits[0].split(' ', 1)[-1])
+        field.label_text = bits[1].split('<')[0]
+        fields.append(field)
+    return {
+        'form': form,
+        'fields': fields,
+    }
